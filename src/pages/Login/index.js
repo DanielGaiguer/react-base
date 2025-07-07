7765e453d906c715257343b542fd011d8b8f662e
Daniel Gaiguer <gaiguer2019@gmail.com>
2025-07-07 11:15:17 -0300

    incluindo axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,18 +3,7 @@ import React from "react";
 import { Title, Paragrafo} from './styled';
 import { Container } from "../../styles/GlobalStyles";
 
-import axios from '../../services/axios';
-
 export default function Login(){
-  React.useEffect(() => {
-    async function getData(){
-        const response = await axios.get('/alunos');
-        console.log(response.data);
-    }
-
-    getData();
-  }, []);
-
   return (
   <Container>
     <Title>
